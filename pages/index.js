1f94d6bdb0b52439fabf74399d54785204bcd22e
Sofi Utami <37136034+sftmm@users.noreply.github.com>
2022-06-07 21:25:43 +0700

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,50 @@
+import Head from "next/head";
+import Layout, { siteTitle } from "../components/layout";
+import utilStyles from "../styles/utils.module.css";
+import Link from "next/link";
+import Date from "../components/date";
+
+import { getSortedPostsData } from "../lib/posts";
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
+
+export default function Home({ allPostsData }) {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>
+          hi I'm sofi, my nickname is daytoy. my pronouns are she/her, i'm 21
+          years old (14/03) and I’m an indonesian. i like anime, keshi, wang
+          yibo, xiao zhan, and listening to music.
+        </p>
+      </section>
+
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              <Link href={`/posts/${id}`}>
+                <a>{title}</a>
+              </Link>
+              <br />
+              {/* <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small> */}
+            </li>
+          ))}
+        </ul>
+      </section>
+    </Layout>
+  );
+}
